52f0523b65aa3556d34249245997331c95f6497c
Maxim <maxim.kalugin.irk@gmail.com@>
2017-02-08 02:20:12 +0800

    устранены баги #3

--- addition.py
+++ addition.py
@@ -3,5 +3,5 @@ def substract(x, y):
 def addition(x, y)
      return int(x) + int(y)
 def multiplication(x, y):
-     return 2*int(x)*int(y)
+     return int(x)*int(y)
 print(int(input())+int(input()))
