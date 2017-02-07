284abf2eb04e2afeb211888fccfe8972b8de5da5
Maxim <maxim.kalugin.irk@gmail.com@>
2017-02-08 02:08:59 +0800

    полная версия

--- addition.py
+++ addition.py
@@ -1,5 +1,7 @@
 def substract(x, y):
      return int(x) - int(y)
+def addition(x, y)
+     return int(x) + int(y)
 def multiplication(x, y):
      return 2*int(x)*int(y)
 print(int(input())+int(input()))
